025ca79012a456ed88ee13d1f430bdd279a358ea
GeekLad <geeklad@geeklad.com>
2024-05-13 07:55:22 -0400

    Added Rug Check exceptions

--- config.ts
+++ config.ts
@@ -2,3 +2,12 @@ export const METEORA_API_URL = "https://dlmm-api.meteora.ag/pair/all";
 export const DEX_SCRENER_API_URL =
   "https://api.dexscreener.com/latest/dex/pairs/solana";
 export const MAX_URL_LENGTH = 1000;
+export const RUG_CHECK_EXCEPTIONS = [
+  "USDC",
+  "SOL",
+  "USDT",
+  "jitoSOL",
+  "bSOL",
+  "JUP",
+  "JupSOL",
+];
